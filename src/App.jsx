c8ecd39847a65c0695f05104547d936ebd0a0a5d
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 16:20:02 -0300

    "test"

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import './App.css'
 const Canvas = React.lazy(() => import("@react-three/fiber").then(module => ({ default: module.Canvas })));
 const Experience = React.lazy(() => import("./features/experience/Experience.jsx").then(module => ({default: module.Experience})))
 function App() {
+    const [data, setData] = useState(false);
     return (
         <>
+            <div>holaaa</div>
+            <button onClick={() => setData(true)}>Click me to see canvas</button>
+            {data &&
                 <Suspense fallback={<div>Loading bitch....</div>}>
                 <Canvas>
                 <Experience/>
                 </Canvas>
                 </Suspense>
+            }
         </>
     )
 }
