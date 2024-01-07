3e3a143563a8b0c09e59e6eb051f6c38645fae2b
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 11:24:16 -0300

    Add an "Enter" button to control Canvas rendering
    Introduced an "Enter" button in App.jsx that sets a state to control rendering of the Canvas enclosing the Experience component. This approach helps reduce unnecessary component rendering until the button is clicked, optimizing site performance.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import {Canvas, useLoader} from "@react-three/fiber";
-import React, {Suspense} from "react";
+import React, {Suspense, useState} from "react";
 import './App.css';
 import {Experience} from "./features/experience/Experience.jsx";
 
 
 
 function App() {
+    const [state, setState] = useState(false);
     return (
         <>
+            <button onClick={() => setState(true)}>
+                ENTER
+            </button>
+            {
+                state &&
                 <Canvas camera={{
                     position: [0, 0, 5],
                     fov: 30,
                 }}>
                     {/* eslint-disable-next-line react/no-unknown-property */}
                     <color attach="background" args={["#ececec"]} />
-                    <Suspense fallback={<div>loading...</div>}>
                         <Experience />
-                    </Suspense>
-
                 </Canvas>
+            }
         </>
     );
 }
