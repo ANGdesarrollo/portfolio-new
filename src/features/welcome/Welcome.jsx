ed784c8d28ece00760db457e2018752f28472fef
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 19:10:15 -0300

    Updated CSS for welcome module and adjusted JSX structure
    The welcome module's CSS and component structure were updated to enhance the layout and improve button appearance. The button component now features more styling attributes and transforms on hover, and these changes are wrapped within a dedicated container in the JSX.

--- src/features/welcome/Welcome.jsx
+++ src/features/welcome/Welcome.jsx
@@ -2,8 +2,13 @@ import style from './welcome.module.css';
 
 export const Welcome = ({handleActiveCanvas}) => {
     return (
+        <div className={style.container}>
+            <div>
                 <button onClick={handleActiveCanvas} className={style.btn} type="button">
                     GO
                 </button>
+            </div>
+
+        </div>
     )
 }
