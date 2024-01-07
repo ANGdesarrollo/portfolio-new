1f2f3017708d57906a560db58354be2e105698e8
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 19:24:04 -0300

    Revamp styling and structure of welcome module
    Improved visual appeal and interaction of welcome module button by enhancing CSS properties. Additionally, adjusted the JSX structure to better accommodate the styling changes. This includes the addition of container divs for a more defined and flexible layout.

--- src/features/welcome/Welcome.jsx
+++ src/features/welcome/Welcome.jsx
@@ -3,12 +3,17 @@ import style from './welcome.module.css';
 export const Welcome = ({handleActiveCanvas}) => {
     return (
         <div className={style.container}>
-            <div>
-                <button onClick={handleActiveCanvas} className={style.btn} type="button">
-                    GO
-                </button>
-            </div>
+            <button className={style.btn} type="button">
+                <strong>SPACE</strong>
+                <div id={style.containerStars}>
+                    <div id={style.stars}></div>
+                </div>
 
+                <div id={style.glow}>
+                    <div className={style.circle}></div>
+                    <div className={style.circle}></div>
+                </div>
+            </button>
         </div>
     )
 }
