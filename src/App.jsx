8258bdf8f13c0cbba4a8785c2a3b3a6c67586300
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 18:29:04 -0300

    Update: Enhance homepage design and functionality
    The homepage has been redesigned with significant changes to the index.html, the creation of new CSS classes for the welcome message, and the implementation of conditional rendering for the Navbar and Welcome components based on the isCanvasActive state. Furthermore, the unnecessary CSS module has been deleted, enhancing the efficiency and maintainability of the code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React, {Suspense} from 'react'
+import {Welcome} from "./features/welcome/Welcome.jsx";
 import {Navbar} from "./features/navbar/template/Navbar.jsx";
+import {useBoolean} from "./features/shared/hooks/useOpenCanvas.js";
 
 const CanvasApp = React.lazy(() => import("./features/canvas/CanvasApp.jsx").then(module => ({default: module.CanvasApp})));
 
 function App() {
+    const { boolean: isCanvasActive, handleBoolean: handleActiveCanvas } = useBoolean();
     return (
         <>
-            <Navbar/>
+            {isCanvasActive && <Navbar/>}
             <main>
-                <Suspense fallback={<div>Loading bitch....</div>}>
-                    <CanvasApp/>
-                </Suspense>
+                {!isCanvasActive ? <Welcome handleActiveCanvas={handleActiveCanvas}/> :
+                    <Suspense fallback={<div>Loading bitch....</div>}>
+                        <CanvasApp/>
+                    </Suspense>
+                }
             </main>
         </>
     )
--- src/features/welcome/Welcome.jsx
+++ src/features/welcome/Welcome.jsx
@@ -1,8 +1,22 @@
-import style from './welcome.module.css';
+import './welcome.css';
+
 export const Welcome = ({handleActiveCanvas}) => {
-        return(
-            <div onClick={handleActiveCanvas} className={style.container}>
-                Welcome to my portfolio
+    return (
+        <div className="welcome-container">
+            <div>
+                <button onClick={handleActiveCanvas} className="btn" type="button">
+                    <strong>GO</strong>
+                    <div id="container-stars">
+                        <div id="stars"></div>
+                    </div>
+
+                    <div id="glow">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </button>
             </div>
-        )
+
+        </div>
+    )
 }
