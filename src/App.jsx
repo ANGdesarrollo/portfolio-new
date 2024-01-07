8ac4e7b6afb03c9709187d9c87b86b4d4e9e4000
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 16:03:36 -0300

    "test"

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import {Canvas, useLoader} from "@react-three/fiber";
-import React, {Suspense, useState} from "react";
-import './App.css';
-import {Experience} from "./features/experience/Experience.jsx";
+import React from 'react'
+import './App.css'
+import { useGLTF } from "@react-three/drei";
 
 
 
 function App() {
-    const [state, setState] = useState(false);
+
     return (
         <>
-            <button onClick={() => setState(true)}>
-                ENTER
-            </button>
-            {
-                state &&
-                <Canvas camera={{
-                    position: [0, 0, 5],
-                    fov: 30,
-                }}>
-                    {/* eslint-disable-next-line react/no-unknown-property */}
-                    <color attach="background" args={["#ececec"]} />
-                        <Experience />
-                </Canvas>
-            }
+            <div>hola</div>
         </>
-    );
+    )
 }
+
 export default App;
