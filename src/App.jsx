8a869ce0076e92ec05aeb787ab1e0f46a9d25a2d
Alexis Graff <alexisgraff123@gmail.com>
2024-01-07 11:20:48 -0300

    feat: Optimize Experience component loading with React.lazy and Suspense
    Use React.lazy for Experience component import in App.jsx for better code splitting and improved performance. Implement React.Suspense to gracefully handle loading states, displaying a fallback UI while the Experience component is loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { Canvas } from "@react-three/fiber";
+import {Canvas, useLoader} from "@react-three/fiber";
 import React, {Suspense} from "react";
 import './App.css';
+import {Experience} from "./features/experience/Experience.jsx";
 
-const Experience = React.lazy(() => import('./features/experience/Experience.jsx'));
+
 
 function App() {
     return (
         <>
-            <Canvas camera={{
-                position: [0, 0, 5],
-                fov: 30,
-            }}>
-                {/* eslint-disable-next-line react/no-unknown-property */}
-                <color attach="background" args={["#ececec"]} />
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Experience />
-                </Suspense>
-            </Canvas>
+                <Canvas camera={{
+                    position: [0, 0, 5],
+                    fov: 30,
+                }}>
+                    {/* eslint-disable-next-line react/no-unknown-property */}
+                    <color attach="background" args={["#ececec"]} />
+                    <Suspense fallback={<div>loading...</div>}>
+                        <Experience />
+                    </Suspense>
+
+                </Canvas>
         </>
     );
 }
