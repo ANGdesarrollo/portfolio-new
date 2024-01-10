2acaabe0b621eb7c841ebd0f608cef4f78fbfc5c
Alexis Graff <alexisgraff123@gmail.com>
2024-01-09 21:19:36 -0300

    Implement Suspense in CanvasApp and App components
    The code has been modified to include React's Suspense function in both the CanvasApp and App.jsx components. This change provides a fallback UI for these components if they are in a 'loading' state, improving user experience by preventing potential blank loading screens.

--- src/features/canvas/CanvasApp.jsx
+++ src/features/canvas/CanvasApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 const Canvas = React.lazy(() => import('@react-three/fiber').then(module => ({ default: module.Canvas })));
 const StarsGeometry = React.lazy(() => import('../stars/Stars.jsx').then(module => ({ default: module.StarsGeometry })));
 const HomeTemplate = React.lazy(() => import('../home/template/HomeTemplate.jsx').then(module => ({ default: module.HomeTemplate })));
@@ -12,8 +12,10 @@ export const CanvasApp = () => {
       }}
     >
       <Provider>
+          <Suspense>
         <StarsGeometry/>
         <HomeTemplate/>
+          </Suspense>
       </Provider>
     </Canvas>
   );
