bb83f96cad28879d430d725c7a1e8af8ae5b5e51
Alexis Graff <alexisgraff123@gmail.com>
2024-01-10 20:54:01 -0300

    Add device detection hook for dynamic star rendering
    Introduced a new hook, `useDetectDevice`, to detect if the user is on a mobile device, and applied this to the `StarsGeometry` component. This will adjust the number and radius of the stars rendered based on the device, optimizing display for mobile users.

---
+++ src/features/shared/hooks/useDetectDevice.js
@@ -0,0 +1,11 @@
+const MOBILE_DEVICES = ['Android', 'webOS', 'iPhone', 'iPad', 'iPod', 'BlackBerry', 'Windows Phone'];
+
+export const useDetectDevice = () => {
+  const device = navigator.userAgent;
+
+  const isMobile = () => {
+    return MOBILE_DEVICES.some(mobileDevice => device.match(new RegExp(mobileDevice, 'i')));
+  };
+
+  return { isMobile };
+};
--- src/features/stars/Stars.jsx
+++ src/features/stars/Stars.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Stars } from '@react-three/drei';
 import { useContext, useEffect, useRef } from 'react';
 import { MyContext } from '../../context/context.jsx';
+import { useDetectDevice } from '../shared/hooks/useDetectDevice.js';
 
 export const StarsGeometry = () => {
   const { handleSetMeshRef } = useContext(MyContext);
   const ref = useRef(null);
+  const { isMobile } = useDetectDevice();
+  const countStars = isMobile() ? 5000 : 20000;
+  const radius = isMobile() ? 25 : 100;
 
   useEffect(() => {
     handleSetMeshRef('stars', ref);
   }, [ref]);
 
-  return <Stars ref={ref} count={20000} fade={true} radius={100}  factor={4} />;
+  return <Stars ref={ref} count={countStars} fade={true} radius={radius}  factor={4} />;
 };
