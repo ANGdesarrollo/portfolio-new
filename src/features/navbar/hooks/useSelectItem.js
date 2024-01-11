b9195005c8a708bf18540fd028167413a0c3aa14
Alexis Graff <alexisgraff123@gmail.com>
2024-01-10 21:32:37 -0300

    Refactor 'useSelectItem' for improved code readability
    Minor changes have been made to 'useSelectItem.js' involving the '+import' line. Spaces were added for better alignment and readability of the 'sectionNames' import statement. This simple adjustment enhances code aesthetics and adherence to JavaScript style guidelines.

--- src/features/navbar/hooks/useSelectItem.js
+++ src/features/navbar/hooks/useSelectItem.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { navItems } from '../constants/data';
 import { useSearchParams } from 'react-router-dom';
-import {sectionNames} from "../../shared/constants/sectionNames.js";
+import { sectionNames } from '../../shared/constants/sectionNames.js';
 export const useSelectItem = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const homeActive = navItems.find(({ item }) => item === sectionNames.home);
