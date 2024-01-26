0954498eae9ee431c5e2c649c08a1648755ea2c2
Mariel Torres <marieltorres97@gmail.com>
2024-01-26 17:19:34 -0300

    feat: added speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { Navbar } from './features/navbar/template/Navbar.jsx';
 import { BrowserRouter } from 'react-router-dom';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const CanvasApp = React.lazy(() => import('./features/canvas/CanvasApp.jsx').then(module => ({ default: module.CanvasApp })));
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <SpeedInsights/>
         <Navbar/>
         <Suspense fallback={<div>Loading bitch....</div>}>
           <CanvasApp/>
