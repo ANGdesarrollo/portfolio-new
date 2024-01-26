2485e58c0600b0abfaaf45a7881ba900b30d94d5
Mariel Torres <marieltorres97@gmail.com>
2024-01-26 17:23:43 -0300

    feat: deleting package of page insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
 import { Navbar } from './features/navbar/template/Navbar.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const CanvasApp = React.lazy(() => import('./features/canvas/CanvasApp.jsx').then(module => ({ default: module.CanvasApp })));
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <SpeedInsights/>
         <Navbar/>
         <Suspense fallback={<div>Loading bitch....</div>}>
           <CanvasApp/>
